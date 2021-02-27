e421468334ae24434d1f1ba83bb16e037cbc7214
Diego Mena <ingelectronicadj@gmail.com>
2021-02-27 18:29:12 -0500

    se carga el bloqueo a capturas de pantalla, impresiones, clic derecho y combinaciones de teclado para copiado, cortado y pegado de información expresamente sobre paginas de moodle referentes a cuestionarios

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # disable-printscreen
 Este es un proyecto abierto que tiene por propósito la creación de un plugin para Moodle que apoye a las instituciones en temas de antiplagio sobre sus cuestionarios. 
+
+1. En este momento para poderlo usar en moodle, vas a ir al bloque de administración e ingresar al apartado de HTML Adicional.
+2. Dentro vas a la sección llamada "Antes de cerrar BODY" y allí copias y pegas el código fuente.  
+```html
+    <!-- disable-printscreen -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script>
+        // Todo el contenido del archivo disablePrintScreen.js
+    </script>
+```
---
+++ disablePrintScreen.js
@@ -0,0 +1,67 @@
+/** PARA MANEJO DE ALERTAS **/
+function lockoutAlert(icon_alert, title_alert, text_alert) {
+    Swal.fire({
+        icon: icon_alert,
+        title: title_alert,
+        text: text_alert,
+        backdrop: true,
+        allowOutsideClick: false,
+        allowEscapeKey: false
+    });
+}
+
+var urlExam = window.location.href;
+// Activa bloqueos si la URL esta asociada a cualquier apartado de cuestionarios en moodle
+if (urlExam.match('mod/quiz')) {
+
+    /** Personzaliza consola ante acceso de herramientas del desarrollador (F12) **/
+    console.error('%cStop!', 'color: red;font-size:2.5rem;font-weight:bold;');  
+    console.warn('%cEsta es una función del navegador destinada a desarrolladores. Si alguien le dijo que copie y pegue algo aquí para habilitar una función o "piratear" la integridad del curso, será reportado y se le bloquerá el acceso a su cuenta', 'font-size:1.25rem;line-height:1.1;'); 
+
+    document.oncontextmenu = function() { // Deshabilita acceso a herramientas del desarrollador por clic derecho
+        lockoutAlert('warning', 'Clic derecho deshabilitado!', 'Estamos trabajando en mejorar el nivel de seguridad de nuestros cuestionarios.');
+        return false;
+    };
+
+    /** Para asegurarse de cancelar eventos **/
+    function cancelEvent() {
+        e.cancelBubble = true;
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    }
+
+    /** Evento a escucha de tecla suelta **/
+    document.addEventListener('keyup', (e) => {
+        if (e.key == 'PrintScreen') { // Deshabilita captura de pantalla --> Tecla (imp pnt)
+            navigator.clipboard.writeText('');
+            lockoutAlert('error', 'Capturas de pantalla deshabilitadas!', 'Solicitamos no intentarlo de nuevo o su acceso será interrumpido y reportado');
+        }
+    });
+
+    /** Evento a escucha de tecla undida **/
+    document.addEventListener('keydown', (e) => {
+        if (e.ctrlKey && e.key == 'p' || e.ctrlKey && e.key == 'P') { // Bloqueo de impresiones --> Comando Ctrl+P
+            lockoutAlert('error', 'Esta sección no se permite imprimir o exportar en PDF', 'Solicitamos no intentarlo de nuevo o su acceso será interrumpido y reportado');
+            cancelEvent();
+        } else if (e.metaKey && e.shiftKey) { // Se sobrepone pantalla ante recorte del Sistema Operativo Windows --> Comando Windows+Shift+S
+            Swal.fire({
+                icon: 'warning',
+                title: 'Recortes de pantalla detectados!',
+                text: 'Solicitamos no intentarlo de nuevo o su acceso será interrumpido y reportado',
+                backdrop: true,
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+                grow: 'fullscreen'
+            });
+        } else if (e.ctrlKey && e.key == 'c' || e.ctrlKey && e.key == 'C') { // Bloqueo de copiado --> Comando Ctrl+C
+            lockoutAlert('error', 'Esta sección no se permite copiar', 'Solicitamos no intentarlo de nuevo o su acceso será interrumpido y reportado');
+            cancelEvent();
+        } else if (e.ctrlKey && e.key == 'x' || e.ctrlKey && e.key == 'X') { // Bloqueo de cortado --> Comando Ctrl+X
+            lockoutAlert('error', 'Esta sección no se permite cortar', 'Solicitamos no intentarlo de nuevo o su acceso será interrumpido y reportado');
+            cancelEvent();
+        } else if (e.ctrlKey && e.key == 'v' || e.ctrlKey && e.key == 'V') { // Bloqueo de pegado --> Comando Ctrl+V
+            lockoutAlert('error', 'En esta sección no se permite pegar información', 'Solicitamos no intentarlo de nuevo o su acceso será interrumpido y reportado');
+            cancelEvent();
+        }
+    });
+}
